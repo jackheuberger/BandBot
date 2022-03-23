cd6a6bb58c8c861acf678be3dfc0d54f6cb4c123
Jack Heuberger <jackheuberger5@gmail.com>
2022-03-23 02:10:43 -0400

    dockerization

--- app.js
+++ app.js
@@ -23,49 +23,62 @@ const app = new App({
 app.command('/chazzadd', async ({ command, ack, say }) => {
     try {
         await ack();
-        const quote = command.text
-        await say({
-            blocks: [
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "Are you sure you want to add the quote \"" + quote + "\" to the list of Chazz quotes?"
-                    }
-                },
-                {
-                    "type": "actions",
-                    "elements": [
-                        {
-                            "type": "button",
-                            "text": {
-                                "type": "plain_text",
-                                "emoji": true,
-                                "text": "Yes"
-                            },
-                            "style": "primary",
-                            "action_id": "add_chazz_quote",
-                            "value": quote
-                        },
-                        {
-                            "type": "button",
-                            "text": {
-                                "type": "plain_text",
-                                "emoji": true,
-                                "text": "No"
-                            },
-                            "style": "danger",
-                            "value": "click_me_123"
+        if(command.text && command.channel_name == 'directmessage') {
+            const quote = command.text
+            await say({
+                blocks: [
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": "_Are you sure you want to add the quote \"" + quote + "\" to the list of Chazz quotes?_ " +
+                                "Make sure you didn't put quotes around it. That'll look super stupid when I already do that for you"
                         }
-                    ]
-                }
-            ]
-        })
+                    },
+                    {
+                        "type": "actions",
+                        "elements": [
+                            {
+                                "type": "button",
+                                "text": {
+                                    "type": "plain_text",
+                                    "emoji": true,
+                                    "text": "Yes"
+                                },
+                                "style": "primary",
+
+                                "action_id": "add_chazz_quote",
+                                "value": quote
+                            },
+                            {
+                                "type": "button",
+                                "text": {
+                                    "type": "plain_text",
+                                    "emoji": true,
+                                    "text": "No"
+                                },
+                                "style": "danger",
+                                "action_id": "no_add_quote"
+                            }
+                        ]
+                    }
+                ]
+            })
+        }
     } catch (error) {
         console.log('err')
         console.error(error);
     }
 });
+
+app.action('no_add_quote', async ({body, ack, say }) => {
+    await ack()
+    say('Cool. Thanks for wasting my time.')
+    await app.client.chat.delete({
+        channel: body.container.channel_id,
+        ts: body.container.message_ts
+    })
+})
 
 app.action('add_chazz_quote', async ({ body, action, ack, say }) => {
     await ack()
@@ -74,6 +87,10 @@ app.action('add_chazz_quote', async ({ body, action, ack, say }) => {
         await appendFile('lists/ChazzQuotes.txt', quote + '\n')
         await say('We Gucci! Quote added! (Or, as Chazz might say, \"' + quote + '\")')
         console.log('quote added successfully')
+        await app.client.chat.delete({
+            channel: body.container.channel_id,
+            ts: body.container.message_ts
+        })
     } catch (err){
         console.error(err)
         say('There was an error! Uh oh!')
@@ -82,7 +99,7 @@ app.action('add_chazz_quote', async ({ body, action, ack, say }) => {
 
 app.event('message', async ({ event, say}) => {
     try {
-        if (event.text.toLowerCase().includes('chazz')) {
+        if (event.channel_type != 'im' && event.text.toLowerCase().includes('chazz')) {
             await say({ text: '\"' + await pullQuote() + '\"' })
         }
     } catch (err) {
